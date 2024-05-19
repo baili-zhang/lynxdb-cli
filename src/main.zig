151ff50a19270922308dd1c76d742bd1fab2b852
zhangbaili <1456938262@qq.com>
2024-05-20 00:22:55 +0800

    Init Project

---
+++ src/main.zig
@@ -0,0 +1,53 @@
+// Copyright 2024 Baili Zhang.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const std = @import("std");
+
+const COPYRIGHT = "LynxDB Client Version {s}\nAll Right Reserved (c) Baili Zhang.\n\n";
+const VERSION = "2024.5.3";
+const PROMPT = "LynxDB-cli>";
+const COLUMN_FAMILY = "Column Family: ";
+const COLUMN = "Column: ";
+const KEY_VALUE_PAIRS = "Key Value Pairs: ";
+const INSERT = "insert";
+const EXIT = "exit";
+const ERROR = "Error: ";
+const INVALID_COMMAND = "Invalid Command";
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.getStdIn().reader();
+
+    try stdout.print(COPYRIGHT, .{VERSION});
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    var isExited: bool = false;
+    while (!isExited) {
+        try stdout.print(PROMPT, .{});
+
+        var line = std.ArrayList(u8).init(allocator);
+        defer line.deinit();
+
+        const writer = line.writer();
+        try stdin.streamUntilDelimiter(writer, '\n', null);
+
+        if (line.items[0] == 'e') {
+            isExited = true;
+        }
+    }
+}
